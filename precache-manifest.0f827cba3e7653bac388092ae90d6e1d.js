6d451ec21d53590e95802f17f59be27a0e95e19f
anton <gavrilenko7732@gmail.com>
2020-06-03 21:30:57 +0300

    Updates

---
+++ precache-manifest.0f827cba3e7653bac388092ae90d6e1d.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "307e078e39175bc7874b3ae184ea0066",
+    "url": "/start_google_page/index.html"
+  },
+  {
+    "revision": "99121a58d0a2ce3ac1bd",
+    "url": "/start_google_page/static/css/main.0e3b1353.chunk.css"
+  },
+  {
+    "revision": "3fd6a805a3b97b61bc1d",
+    "url": "/start_google_page/static/js/2.69bc59d8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/start_google_page/static/js/2.69bc59d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99121a58d0a2ce3ac1bd",
+    "url": "/start_google_page/static/js/main.fdfe2296.chunk.js"
+  },
+  {
+    "revision": "c37c630aa7efa1fd529b",
+    "url": "/start_google_page/static/js/runtime-main.c9ff520d.js"
+  },
+  {
+    "revision": "22673265a6f2f7200da40c659c9129ae",
+    "url": "/start_google_page/static/media/chrome.22673265.svg"
+  },
+  {
+    "revision": "c4cb17e1b6783ff8a21367ad15365a82",
+    "url": "/start_google_page/static/media/gmail.c4cb17e1.svg"
+  },
+  {
+    "revision": "976b169e571fefe96613efd9d9988272",
+    "url": "/start_google_page/static/media/google-calendar.976b169e.svg"
+  },
+  {
+    "revision": "7c9e1c054c7c9b90b7eb257b01888f3a",
+    "url": "/start_google_page/static/media/google-contacts.7c9e1c05.svg"
+  },
+  {
+    "revision": "b0f53ab22e2111594dfd60be66bce389",
+    "url": "/start_google_page/static/media/google-docs.b0f53ab2.svg"
+  },
+  {
+    "revision": "9b48ea1d6c307ea4dcf37ed6d7e49327",
+    "url": "/start_google_page/static/media/google-drive.9b48ea1d.svg"
+  },
+  {
+    "revision": "6755ae9eaa299a60e014344dd2c2b274",
+    "url": "/start_google_page/static/media/google-duo.6755ae9e.svg"
+  },
+  {
+    "revision": "3a0c4642cd941bf65e19b2f0986678e7",
+    "url": "/start_google_page/static/media/google-hangouts.3a0c4642.svg"
+  },
+  {
+    "revision": "94d0d6209907ce147cb27e834351f1ee",
+    "url": "/start_google_page/static/media/google-keep.94d0d620.svg"
+  },
+  {
+    "revision": "101496604684ee4cc4367644173e65be",
+    "url": "/start_google_page/static/media/google-maps.10149660.svg"
+  },
+  {
+    "revision": "3e74a92b984725abffea93f2bd6b501e",
+    "url": "/start_google_page/static/media/google-photos.3e74a92b.svg"
+  },
+  {
+    "revision": "88c54782480ce9d6136328e6c501fd6c",
+    "url": "/start_google_page/static/media/google-play.88c54782.png"
+  },
+  {
+    "revision": "8e07eddb6b5cc33564a3543f15c83bc5",
+    "url": "/start_google_page/static/media/google_acc.8e07eddb.svg"
+  },
+  {
+    "revision": "82ed5e7c69c579ad17aeac4ce06a7331",
+    "url": "/start_google_page/static/media/google_b.82ed5e7c.svg"
+  },
+  {
+    "revision": "f5b006e368c7c897313507cdd3f781cd",
+    "url": "/start_google_page/static/media/google_translate.f5b006e3.svg"
+  },
+  {
+    "revision": "741061e07e22354ce1ac910ae76ca280",
+    "url": "/start_google_page/static/media/hangouts-meet.741061e0.svg"
+  },
+  {
+    "revision": "c9464db3ca0743d1cdaaed40ba91805e",
+    "url": "/start_google_page/static/media/jamboard.c9464db3.svg"
+  },
+  {
+    "revision": "80fa4bcab0351fdccb69c66fb55dcd00",
+    "url": "/start_google_page/static/media/logo.80fa4bca.png"
+  },
+  {
+    "revision": "a51ab7ed010a415170cd93a2a29d96dd",
+    "url": "/start_google_page/static/media/youtube.a51ab7ed.svg"
+  }
+]);
